712e97496882d4b2fe3e24d103012c46f1827e7c
Yubraj Ghimire <yghimire@gmail.com>
2019-08-09 20:46:14 -0500

    product supplier

---
+++ src/main/java/com/tanab/product/prodcust/ProdcustApplication.java
@@ -0,0 +1,13 @@
+package com.tanab.product.prodcust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdcustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdcustApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tanab/product/prodcust/ProdcustApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tanab.product.prodcust;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProdcustApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
